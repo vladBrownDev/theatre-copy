af9525d0ae5a53d185f87c5f2ebe44c6c2b08887
spyhere <gekav3@mail.ru>
2022-10-05 16:09:57 +0300

    Handle per-page layouts in `_app.tsx`

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
-import '../styles/globals.css'
+import type { ReactElement, ReactNode } from 'react'
+import type { NextPage } from 'next'
 import type { AppProps } from 'next/app'
+import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode
 }
 
-export default MyApp
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  if (Component.getLayout) {
+    const getLayout = Component.getLayout
+    return getLayout(<Component {...pageProps} />)
+  }
+
+  return (<Component {...pageProps} />)
+}
