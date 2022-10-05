499d16ae69c758b30b181da2e6a25af1ab39e045
spyhere <gekav3@mail.ru>
2022-10-05 23:57:49 +0300

    Connect ApolloProvider for the app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,11 @@
-import type { ReactElement, ReactNode } from 'react'
-import type { NextPage } from 'next'
-import type { AppProps } from 'next/app'
-import '../styles/globals.css'
+import type { ReactElement, ReactNode } from "react"
+import type { NextPage } from "next"
+import type { AppProps } from "next/app"
+import { ApolloProvider } from "@apollo/client"
+import client from "../src/api"
+import "../styles/globals.css"
 
-export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+export type NextPageWithLayout<P = Record<string, unknown>, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode
 }
 
@@ -12,10 +14,17 @@ type AppPropsWithLayout = AppProps & {
 }
 
 export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  let componentWithLayout
   if (Component.getLayout) {
     const getLayout = Component.getLayout
-    return getLayout(<Component {...pageProps} />)
+    componentWithLayout = getLayout(<Component {...pageProps} />)
+  } else {
+    componentWithLayout = <Component {...pageProps} />
   }
 
-  return (<Component {...pageProps} />)
+  return (
+    <ApolloProvider client={client}>
+      {componentWithLayout}
+    </ApolloProvider>
+  )
 }
