01e8e4ccf5def8c3645d73223eeaa1d1ed9b212b
spyhere <gekav3@mail.ru>
2022-10-07 21:58:35 +0300

    Put valid endpoint for apollo client

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,10 +2,12 @@ import {
   ApolloClient,
   InMemoryCache
 } from "@apollo/client"
+import { API_ENDPOINT } from "../constants/constants"
 
 const client = new ApolloClient({
-  uri: process.env.API_ENDPOINT,
-  cache: new InMemoryCache()
+  uri: API_ENDPOINT,
+  cache: new InMemoryCache(),
+  ssrMode: typeof "window" === undefined,
 })
 
 export default client
